2292b4ad41ba345017652256b5bde809e1a9ff19
Jose Alberto Ruiz-Carrillo González <jruiz@bancocaminos.es>
2020-01-29 15:05:49 +0100

    BoundsVsFrame sort explanation added

--- BoundsVSFrame.playground/Contents.swift
+++ BoundsVSFrame.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
 
+//BOUNDS VS FRAME
+
+// Frame: Teniendo una UIView de ejemplo, el frame es su posicón relativa a su super view o vista padre.
+let view = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 60, height: 60))
+
+// Bounds: Teniendo una UIView de ejemplo, el bounds es su posición relativa a si misma. El origen por defecto de los ejes x,y serían 0,0. Esto es la esquina superior izquierda.
+view.frame.origin = CGPoint(x: 0.0, y: 0.0)
