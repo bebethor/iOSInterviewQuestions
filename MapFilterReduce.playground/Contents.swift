56818ae3820e1ce73c214e6e91ccd7a419bf572b
Jose Alberto Ruiz-Carrillo González <jruiz@bancocaminos.es>
2020-01-29 11:38:40 +0100

    Added Map code and explanation

--- MapFilterReduce.playground/Contents.swift
+++ MapFilterReduce.playground/Contents.swift
@@ -18,7 +18,7 @@ var myAppleTv      = Device(type: "Apple TV", price: 200.00, color: "Black")
 
 let myDevices = [myiMacPro, myiPhoneXS, myiPhone11, myIpad, myAppleWatch, myAppleTv]
 
-// FILTER: Con la función filter tenemos la opción de seleccionar de un array determinados elementos en función de una condición y obtener como resultado solamente aquellos que cumpla dicha condición. Esto es aplicable a cualquier tipo de condición cuyo resultado sea true o false, lo que nos abre posibilidades a multitud de opciones.
+// FILTER:Filtrando. Con la función filter tenemos la opción de seleccionar de un array determinados elementos en función de una condición y obtener como resultado solamente aquellos que cumpla dicha condición. Esto es aplicable a cualquier tipo de condición cuyo resultado sea true o false, lo que nos abre posibilidades a multitud de opciones.
 
 //Sacamos todos los dispositivos que sean iphone de la forma clásica con un bucle for
 var myPhones: [Device] = []
@@ -35,4 +35,7 @@ let iPhones = myDevices.filter({ return $0.type == "iPhone"}) // en este caso $0
 print(iPhones)
 
 
-//Commit de prueba
+// MAP: transformando. La función map lo que hace es ejecutar una función en todos los elementos de un array, donde construimos dicha función de transformación en base a la referencia genérica de los valores. Es decir, dentro de la llamada a map, la referencia $0 representa el dato en sí dentro nuestro array.
+
+let canadianPrices: [Float] = myDevices.map({ return $0.price * 1.2 })
+print(canadianPrices)
