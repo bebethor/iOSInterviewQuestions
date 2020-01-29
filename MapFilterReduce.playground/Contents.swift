f09f01c46b8064834291f90eabbf0f36f3a39c26
Jose Alberto Ruiz-Carrillo González <jruiz@bancocaminos.es>
2020-01-29 10:49:37 +0100

    ADDED: Filter code and explanation

--- MapFilterReduce.playground/Contents.swift
+++ MapFilterReduce.playground/Contents.swift
@@ -1,4 +1,36 @@
 import UIKit
 
 
+//Filter, Map, Reduce
+
+struct Device {
+    var type: String
+    var price: Float
+    var color: String
+}
+
+var myiMacPro      = Device(type: "iMac Pro", price: 4999.00, color: "Space Grey")
+var myiPhoneXS     = Device(type: "iPhone", price: 799.0, color: "Black")
+var myiPhone11     = Device(type: "iPhone", price: 1000.00, color: "Space Grey")
+var myIpad         = Device(type: "iPad", price: 400, color: "Space Grey")
+var myAppleWatch   = Device(type: "Apple Watch", price: 500.00, color: "Space Grey")
+var myAppleTv      = Device(type: "Apple TV", price: 200.00, color: "Black")
+
+let myDevices = [myiMacPro, myiPhoneXS, myiPhone11, myIpad, myAppleWatch, myAppleTv]
+
+// FILTER: Con la función filter tenemos la opción de seleccionar de un array determinados elementos en función de una condición y obtener como resultado solamente aquellos que cumpla dicha condición. Esto es aplicable a cualquier tipo de condición cuyo resultado sea true o false, lo que nos abre posibilidades a multitud de opciones.
+
+//Sacamos todos los dispositivos que sean iphone de la forma clásica con un bucle for
+var myPhones: [Device] = []
+for device in myDevices {
+    if device.type == "iPhone" {
+        myPhones.append(device)
+    }
+}
+ 
+print(myPhones)
+
+//Ahora con filter
+let iPhones = myDevices.filter({ return $0.type == "iPhone"}) // en este caso $0 corresponde a la variable devie del bucle for anterior
+print(iPhones)
 
