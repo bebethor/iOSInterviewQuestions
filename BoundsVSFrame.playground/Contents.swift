c6205835a31b80ef6a44c551e95337704523d64a
Jose Alberto Ruiz-Carrillo González <jruiz@bancocaminos.es>
2020-01-29 15:07:32 +0100

    stackoverflow documentation url

--- BoundsVSFrame.playground/Contents.swift
+++ BoundsVSFrame.playground/Contents.swift
@@ -2,6 +2,7 @@ import UIKit
 
 
 //BOUNDS VS FRAME
+//https://stackoverflow.com/questions/1210047/cocoa-whats-the-difference-between-the-frame-and-the-bounds/28917673#28917673
 
 // Frame: Teniendo una UIView de ejemplo, el frame es su posicón relativa a su super view o vista padre.
 let view = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 60, height: 60))
