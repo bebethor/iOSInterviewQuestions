08b626af33dc418ca3a4e0a4e0d23450d6fc2595
Jose Alberto Ruiz-Carrillo González <jruiz@bancocaminos.es>
2020-02-03 14:23:14 +0100

    DELETED: Code

--- Notifications-Observers/Delegates-Protocols/BaseScreen.swift
+++ Notifications-Observers/Delegates-Protocols/BaseScreen.swift
@@ -17,17 +17,8 @@ class BaseScreen: UIViewController {
     @IBOutlet weak var chooseButton: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
     
-    let light = Notification.Name(rawValue: lightNotificationKey)
-    let dark = Notification.Name(rawValue: darkNotificationKey)
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        chooseButton.layer.cornerRadius = chooseButton.frame.size.height/2
-        createObservers()
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -36,50 +27,22 @@ class BaseScreen: UIViewController {
     
     func createObservers() {
         
-        //Light
-        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateCharacterImage(notification:)), name: light, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateNameLabel(notification:)), name: light, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateBackground(notification:)), name: light, object: nil)
-        
-        //Dark
-        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateCharacterImage(notification:)), name: dark, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateNameLabel(notification:)), name: dark, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateBackground(notification:)), name: dark, object: nil)
     }
     
     @objc func updateCharacterImage(notification: NSNotification) {
         
-        let isLight = notification.name == light
-        let image = isLight ? UIImage(named: "luke")! : UIImage(named: "vader")!
-        mainImageView.image = image
     }
     
     @objc func updateNameLabel(notification: NSNotification) {
-        
-        let isLight = notification.name == light
-        let name = isLight ? "Luke Skywalker" : "Darth Vader"
-        nameLabel.text = name
         
     }
     
     @objc func updateBackground(notification: NSNotification) {
         
-        let isLight = notification.name == light
-        let color = isLight ? UIColor.cyan : UIColor.red
-        view.backgroundColor = color
     }
     
     
     @IBAction func chooseButtonTapped(_ sender: UIButton) {
-        let selectionVC = storyboard?.instantiateViewController(withIdentifier: "SelectionScreen") as! SelectionScreen
-        present(selectionVC, animated: true, completion: nil)
+        
     }
-    
-    
-    
-    
-    
-    
-    
-    
 }
--- Notifications-Observers/Delegates-Protocols/SelectionScreen.swift
+++ Notifications-Observers/Delegates-Protocols/SelectionScreen.swift
@@ -15,14 +15,10 @@ class SelectionScreen: UIViewController {
     }
 
     @IBAction func imperialButtonTapped(_ sender: UIButton) {
-        let name = Notification.Name(rawValue: darkNotificationKey)
-        NotificationCenter.default.post(name: name, object: nil)
-        dismiss(animated: true, completion: nil)
+        
     }
 
     @IBAction func rebelButtonTapped(_ sender: UIButton) {
-        let name = Notification.Name(rawValue: lightNotificationKey)
-        NotificationCenter.default.post(name: name, object: nil)
-        dismiss(animated: true, completion: nil)
+        
     }
 }
