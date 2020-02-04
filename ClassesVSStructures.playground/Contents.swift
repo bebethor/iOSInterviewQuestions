33961ea807550b6adb3de5e173bba1f74ed57126
Jose Alberto Ruiz-Carrillo González <jruiz@bancocaminos.es>
2020-02-04 14:39:46 +0100

    ADDDED : Code and explanation

--- ClassesVSStructures.playground/Contents.swift
+++ ClassesVSStructures.playground/Contents.swift
@@ -1,3 +1,38 @@
 import UIKit
 
-var str = "Hello, playground"
+
+/*
+ CLASSES VS STRUCTURES
+ */
+
+
+// CLASSES: Are reference types
+class MacBook {
+    var year: Int
+    var color: String
+    
+    init(year: Int, color: String) {
+        self.year = year
+        self.color = color
+    }
+}
+
+let myMacBook = MacBook(year: 2020, color: "Space grey")
+let stolenMacBook = myMacBook
+
+stolenMacBook.color = "Spraypainted green"
+
+print(myMacBook.color)
+
+
+// Structs are value types. Struct do not need init.
+struct Iphone {
+    var number: Int
+    var color: String
+}
+
+let myIphone = Iphone(number: 10, color: "Black")
+var stolenIphone = myIphone
+stolenIphone.color = "Pink"
+print(myIphone.color)
+
